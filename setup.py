a806198636a79c0a02b149d586799c6e6f49259d
MaxsimSmirnov <mvad.smirnov@gmail.com>
2024-04-15 20:06:30 +0300

    Init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Small organic molecules' potentials modelling in different solvents',
+    author='Maxim Smirnov',
+    license='MIT',
+)
