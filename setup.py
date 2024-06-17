2ed0dc52b489f2d1eb5074e7a9baae0e0d2e4878
Maxim Smirnov <152451024+makeximus@users.noreply.github.com>
2024-06-17 15:55:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Small organic molecules' potentials modelling in different solvents',
+    description='Small organic molecules' potentials modelling',
     author='Maxim Smirnov',
     license='MIT',
 )
